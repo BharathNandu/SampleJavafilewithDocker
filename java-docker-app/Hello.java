c177f64bfe3b7e5121f73ac4295e4949f4c96f19
Bharath <bharath.nandu9@gmail.com>
2024-07-16 20:56:57 +0530

    new version

--- java-docker-app/Hello.java
+++ java-docker-app/Hello.java
@@ -2,5 +2,6 @@ class Hello{
 
 public static void main(String[] args){  
         System.out.println("This is java app \n by using Docker run");  
+        System.out.println("This is a new version app");
     }  
 }  
